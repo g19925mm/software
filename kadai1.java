43c49f9b1fcf639a6c8ca0b551a0312eb2cbe066
g19925mm <75403433+g19925mm@users.noreply.github.com>
2021-04-18 13:09:37 +0900

    Update kadai1.java

--- kadai1.java
+++ kadai1.java
@@ -1,6 +1,6 @@
 public class kadai1{
   public static void main(String[] args) {
-    System.out.println("orange");
-    System.out.println("美味しいから");
+    System.out.println("みかん");
+    System.out.println("食べやすく、食べるとすっきりするから。");
   }
 }
